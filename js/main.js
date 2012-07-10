90338c795a7fe58220bf6e4c620c518d46790790
Adam Vinsant <agvinsant@fullsail.edu>
2012-07-09 22:02:35 -0400

    Made some structural changes in html. Added many more css properties to both pages.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,24 @@
 // My Honey Do List js file
 // Activity 2
 
+window.addEventListener("DOMContentLoaded", function(){
+	
+	// getElementById Function
+	function a(id) {
+		var theElement = document.getElementById(id);
+		return theElement;
+	};
+	
+	// id variables
+	var ct = a('choretype'),
+		cn = a('chorename'),
+		urg= a('urgent'),
+		fin= a('finishby'),
+		dif= a('difficulty'),
+		notes= a('chorenotes');
+	
+		 		
+	
+	
+	
+});
