ac50c6d3cd9f3234376df6efe029e3766685ddd5
Adam Vinsant <agvinsant@fullsail.edu>
2012-07-10 18:13:06 -0400

    Changed the fonts after viewing the app in the ios simulator. Didn't like it. Also added an About this app section on the bottom of the index page. Changed some style points.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	};
 	
 	// id variables
-	var	opt= [blank, inside, outside, errand],
+	var	opt= ["blank", "inside", "outside", "errand"],
 		ct = a('choretype'),
 		cn = a('chorename'),
 		urg= a('urgent'),
@@ -20,6 +20,25 @@ window.addEventListener("DOMContentLoaded", function(){
 		dif= a('difficulty'),
 		notes= a('chorenotes');
 		
+	// Creating Select elements and populate with options
+	function makeType () {
+		var 	formTag = document.getElementsByTagName("form"),
+			selectLi = a('select'),
+			makeSelect = document.createElement('select');
+			makeSelect.setAttribute('id', 'type');
+		
+		for (var i=0; j=opt.length, i<j; i++){
+			var makeOption = document.createElement('types');
+			var optText = opt[i];
+			makeOption.setAttribute("value", optText);
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		};
+		selectLi.appendChild(makeSelect);
+	};
+	makeType();
+	
+	// Button Action functions
 	var displayButton = a('display');
 	displayButton.addEventListener("click", getData);
 	
@@ -28,9 +47,5 @@ window.addEventListener("DOMContentLoaded", function(){
 	
 	var submitButton = a('submitbutton');
 	submitButton.addEventListener = a("click", storeData);
-	
-		 		
-	
-	
 	
 });
