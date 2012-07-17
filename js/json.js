b40763935eaf4f2f31db7b131bab9a42b8fd237a
Adam Vinsant <agvinsant@fullsail.edu>
2012-07-16 23:32:31 -0400

    Changed font and button sizes in css on both files. Inside js file, added a bunch of comments pointing to things that need to be changed when I reference these functions again for other lists. I will be adding more lists such as contact and others to app for full out distribution.

---
+++ js/json.js
@@ -0,0 +1,11 @@
+var json = {
+    "chore1": {
+        "choretype": ["Chore Type: ", "Inside Job"],
+        "chorename": ["Name: ", "Clean Kitchen"],
+        "finishby": ["Finish By: ", "12/31/2012"],
+        "urgency":["Is this chore urgent: ", "No"],
+        "difficulty":["Difficulty: ", "50"],
+        "chorenotes":["Chore Notes: ", "Notes about cleaning kitchen."]
+    }
+        
+}
